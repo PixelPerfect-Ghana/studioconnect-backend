8f21323dc19ddd3dd0b662da4337a26be9b81e09
linda <lindabrokye1993@gmail.com>
2024-11-14 12:52:25 +0000

    name added

--- validators/user_validator.js
+++ validators/user_validator.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 export const registerUserValidator = Joi.object({
-    name: Joi.string().required(),
+    Name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
     role:Joi.string().valid('user','vendor')
